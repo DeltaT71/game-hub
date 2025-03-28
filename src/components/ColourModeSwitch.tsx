246c8b2569628a998901084d9bb41337d8231c70
DeltaT71 <DeltaT71@gmail.com>
2025-03-28 18:45:04 +0200

    Added Dark mode toggle

---
+++ src/components/ColourModeSwitch.tsx
@@ -0,0 +1,7 @@
+import { ColorModeButton } from "../components/ui/color-mode";
+
+const ColourModeSwitch = () => {
+  return <ColorModeButton></ColorModeButton>;
+};
+
+export default ColourModeSwitch;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider.tsx";
+import { Provider } from "./components/ui/provider";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
