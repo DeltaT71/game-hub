2368311f16d4913defb50c2f4f936fc5d026ea76
DeltaT71 <DeltaT71@gmail.com>
2025-04-12 19:03:43 +0300

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,42 @@
 import { Button, Menu } from "@chakra-ui/react";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-release", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
+
   return (
     <Menu.Root>
       <Menu.Trigger asChild>
         <Button variant="outline" size="sm">
-          Order by: Relevance
+          {currentSortOrder?.label || "Relevence"}
         </Button>
       </Menu.Trigger>
       <Menu.Positioner>
         <Menu.Content>
-          <Menu.Item value="">Relevance</Menu.Item>
-          <Menu.Item value="">Date added</Menu.Item>
-          <Menu.Item value="">Name</Menu.Item>
-          <Menu.Item value="">Release date</Menu.Item>
-          <Menu.Item value="">Popularity</Menu.Item>
-          <Menu.Item value="">Average rating</Menu.Item>
+          {sortOrders.map((order) => (
+            <Menu.Item
+              onClick={() => onSelectSortOrder(order.value)}
+              key={order.value}
+              value={order.value}
+            >
+              {order.label}
+            </Menu.Item>
+          ))}
         </Menu.Content>
       </Menu.Positioner>
     </Menu.Root>
