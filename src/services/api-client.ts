1f1baa602ff0ff7136aa914fdbaccdef027cf7eb
DeltaT71 <DeltaT71@gmail.com>
2025-04-15 18:42:36 +0300

    Moving the new API key to .env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const apiKey = import.meta.env.VITE_API_KEY;
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f24d58e2568d41dc9886c13bd836cd12",
+    key: apiKey,
   },
 });
